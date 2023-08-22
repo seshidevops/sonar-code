aba8871b108beb6b9b3d05a0621c98307c7eafea
seshidevops <rekhaseshi222@gmail.com>
2023-08-22 22:33:46 +0530

    adding files

---
+++ src/main/java/com/maaven/practice/App.java
@@ -0,0 +1,15 @@
+package com.maaven.practice;
+
+/**
+ * Hello world!
+ *
+ */
+pubsssslic class App 
+{
+    public static void main( String[] args )
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
